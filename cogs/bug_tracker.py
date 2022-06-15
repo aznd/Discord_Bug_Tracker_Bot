57cd4bfea6cb5c54c1422e01957a3519935b1534
aznd <80119822+aznd@users.noreply.github.com>
2022-06-15 20:55:27 +0200

    Add colors to embed and fix bug on which message couldnt find bug would be sent even if not needed

--- cogs/bug_tracker.py
+++ cogs/bug_tracker.py
@@ -12,7 +12,9 @@ class BugTracker(commands.Cog):
     @commands.command()
     @commands.has_role("Dev")
     async def create(
-            self, ctx: discord.ext.commands.Context, title=None,
+        self,
+        ctx: discord.ext.commands.Context,
+        title=None,
     ):
         if not ctx.message.reference:
             await ctx.send("You can only use this command in a reply.")
@@ -26,28 +28,46 @@ class BugTracker(commands.Cog):
                 )
                 return
             thread = await ctx.message.create_thread(name=title)
-            embed = discord.Embed(title=title,
-                                  description=f"Jump to thread: https://discord.com/channels/{ctx.guild.id}/{thread.id}")
+            embed = discord.Embed(
+                title=title,
+                description=f"Jump to thread: https://discord.com/channels/{ctx.guild.id}/{thread.id}",
+                color=0x38A493,
+            )
             await channel.send(embed=embed)
 
     @commands.command()
     @commands.has_role("Dev")
     async def close(self, ctx: discord.ext.commands.Context):
         thread = ctx.channel
-
+        is_match = False
         if type(thread) == discord.Thread:
             channel = self.client.get_channel(bug_channel_id)
+
             async for message in channel.history():
                 if str(thread.id) in message.embeds[0].description:
+
+                    """
+                    If the current thread id is in the description of the embed,
+                    e.g.
+                    """
+                    is_match = True
                     old_title = message.embeds[0].title
                     old_description = message.embeds[0].description
                     await message.delete()
                     if "- CLOSED" in old_title:
                         old_title = old_title.replace(" - CLOSED", "")
-                    new_embed = discord.Embed(title=f"{old_title} - CLOSED", description=old_description)
+                    new_embed = discord.Embed(
+                        title=f"{old_title} - CLOSED",
+                        description=old_description,
+                        color=0xCC7A94,
+                    )
                     await channel.send(embed=new_embed)
-                else:
-                    await ctx.send("Could not find bugs in #dev-bugs! Please edit manually.")
+                    break
+            if not is_match:
+                await ctx.send(
+                    "Could not find bugs in #dev-bugs! Please edit manually."
+                )
+                return
             await ctx.send("Bug has been closed.")
             await thread.edit(archived=True, locked=True)
         else:
@@ -64,11 +84,15 @@ class BugTracker(commands.Cog):
                     title = str(message.embeds[0].title).replace(" - CLOSED", "")
                     old_description = message.embeds[0].description
                     await message.delete()
-                    new_embed = discord.Embed(title=title, description=old_description)
+                    new_embed = discord.Embed(
+                        title=title, description=old_description, color=0x38A493
+                    )
                     await channel.send(embed=new_embed)
                     await ctx.send("Bug has been re-opened.")
                 else:
-                    await ctx.send("Could not find bugs in #dev-bugs! Please edit manually.")
+                    await ctx.send(
+                        "Could not find bugs in #dev-bugs! Please edit manually."
+                    )
         else:
             await ctx.send("This command can only be used in a thread.")
 
